ad56157862eb2278bb12a744d091b598cd305f4a
toriboone <vkishman88@gmail.com>
2017-11-13 13:40:23 -0500

    first draft

---
+++ rockpaperscissors.js
@@ -0,0 +1,64 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log('Not a valid input.');
+   };
+};
+
+const getComputerChoice = () => {
+  randomNumber = Math.floor(Math.random() * 3);
+
+  switch (randomNumber){
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return 'scissors';
+      break;
+                      }
+}
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'It\'s a tie!';
+  }
+
+  if (userChoice === 'rock') {
+    if (computerChoice === 'scissors') {
+      return 'You won!';
+    } else {
+      return 'Computer won!';
+    }
+  }
+
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'paper') {
+      return 'You won!';
+    } else {
+      return 'Computer won!';
+    }
+  }
+
+  if (userChoice === 'paper') {
+    if (computerChoice === 'rock') {
+      return 'You won!';
+    } else {
+      return 'Computer won!';
+    }
+  }
+}
+
+const playGame = () => {
+  let userChoice = getUserChoice('paper');
+  let computerChoice = getComputerChoice();
+  console.log(`You choose ${userChoice}. Computer chooses ${computerChoice}.`);
+
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
